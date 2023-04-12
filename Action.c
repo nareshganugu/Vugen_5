55663a4607883da1a72f4025acc8286630b1df2c
nganugu1 <naresh.ganugu1@T-Mobile.com>
2023-04-12 16:47:02 -0400

    edit message

--- Action.c
+++ Action.c
@@ -2,13 +2,14 @@ Action()
 {
 	
 	
-	lr_start_transaction("S01_SCEE_TC03_Check_Eligibility");
+	lr_start_transaction("T01_'Test_Message");
 	
 
 	lr_output_message("Successfully edited");
 	
     
-   	lr_end_transaction("S01_SCEE_TC03_Check_Eligibility", LR_FAIL);
+   lr_end_transaction("T01_'Test_Message", LR_AUTO);
+
    	
 
 	
