ab4e2ba58322209344ea683cbbd4a72a1c2d57ee
Naresh Personal Email <nareshganugu@Gmail.com>
2023-05-24 17:52:40 -0400

    edited

--- Action.c
+++ Action.c
@@ -5,7 +5,7 @@ Action()
 	lr_start_transaction("T01_'Test_Message");
 	
 
-	lr_output_message("Successfully edited");
+	lr_output_message("Successfully edit");
 	
     
    lr_end_transaction("T01_'Test_Message", LR_AUTO);
