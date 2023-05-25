dc90620a7854728e4547377cb9dce0ca68508173
Naresh Personal Email <naresh.ganuggu1@t-mobile.com>
2023-05-25 11:34:48 -0400

    hello message

--- Action.c
+++ Action.c
@@ -5,7 +5,7 @@ Action()
 	lr_start_transaction("T01_'Test_Messages");
 	
 
-	lr_output_message("Successfull");
+	lr_output_message("Hello");
 	
     
    lr_end_transaction("T01_'Test_Messages", LR_AUTO);
