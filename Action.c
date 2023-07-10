da9a3a2c4bed15fa22d170e64779c0c501da66b5
Naresh Personal Email <nareshganugu@gmail.com>
2023-07-10 12:50:52 -0400

    Added_TransactionName

--- Action.c
+++ Action.c
@@ -1,12 +1,15 @@
 Action()
 {
 		
-	
+		lr_start_transaction("T01");
+
 		web_rest("GET: http://www.tmo.com",
 		"URL=http://www.tmo.com",
 		"Method=GET",
 		"Snapshot=t117709.inf",
 		LAST);
+		
+		lr_end_transaction("T01", LR_AUTO);
 
 	
 	
